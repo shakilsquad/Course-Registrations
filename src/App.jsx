164a149fbd24803769f6155b498ec529bacd7f46
shakilsquad <freelancershakilsquad@gmail.com>
2023-11-05 23:11:42 +0600

    faveIcon add

--- src/App.jsx
+++ src/App.jsx
@@ -127,15 +127,17 @@ function App() {
     <div className="px-4 py-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 lg:py-14">
       <div className=" ">
         <h1 className="font-bold text-[32px] text-center">
-          Course Registrations
+          Course Registration
         </h1>
         {/* all course */}
+        
         <div className="grid grid-cols-4 gap-4 mt-8">
           <div className="col-span-3 grid grid-cols-3 gap-4">
             {data.map((itm) => (
               <CourseCart addProduct={addProduct} itm={itm} key={itm.id} />
             ))}
           </div>
+
           <div className="">
             <div className="bg-[white] p-5 rounded-lg">
               <h2 className="text-[#2F80ED] text-lg rmn font-semibold pb-4 border-b">
